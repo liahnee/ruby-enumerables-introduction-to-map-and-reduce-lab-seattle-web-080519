475ea67d737f0b8fd73aff3ff5c1489b3e18710a
Catherine Yang <liahnee@gmail.com>
2019-07-19 02:13:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,8 @@
 # My Code here....
 def map_to_negativize(source_array)
-
-  return 
+  source_array.length.times {|n| 
+    source_array[n] = source_array[n]-1}
+  return source_array
 end
 
 def map_to_no_change(source_array)
