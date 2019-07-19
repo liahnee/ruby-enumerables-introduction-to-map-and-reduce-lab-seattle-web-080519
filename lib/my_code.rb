1db072d701575364a47fd2ee40c63c15cb4bc36c
Catherine Yang <liahnee@gmail.com>
2019-07-19 02:15:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,17 +6,24 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-
-  return 
+  source_array.length.times {|n| 
+    source_array[n] = source_array[n]}
+  return source_array
 end
 
 def map_to_double(source_array)
-
-  return 
+  source_array.length.times {|n| 
+    source_array[n] = source_array[n]*2}
+  return source_array
 end
 
 def map_to_square(source_array)
-
-  return 
+  source_array.length.times {|n| 
+    source_array[n] = source_array[n]**2}
+  return source_array
 end
 
+def reduce_to_total(source_array)
+  
+  return source_array
+end 
